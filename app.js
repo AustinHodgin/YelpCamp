e09a2247540ca4b826bf8ed6c5420189ff87ff37
AustinHodgin <hodgina@oregonstate.edu>
2020-01-13 18:54:04 -0800

    added connection to external mongodb site

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 //Package Includes
-const express           = require("express");
-const bodyParser        = require("body-parser");
-const mongoose          = require("mongoose");
-const methodOverride    = require("method-override");
-const Campground        = require("./models/campground");
-const passport          = require("passport");
-const LocalStrategy     = require("passport-local");
-const User              = require("./models/user");
-const Comment           = require("./models/comment");
-const seedDB            = require("./seeds");
-const flash             = require("connect-flash");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const Campground = require("./models/campground");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+const Comment = require("./models/comment");
+const seedDB = require("./seeds");
+const flash = require("connect-flash");
 
 //Route Includes
-var commentRoutes       = require("./routes/comments");
-var campgroundRoutes    = require("./routes/campgrounds");
-var indexRoutes         = require("./routes/index");
+var commentRoutes = require("./routes/comments");
+var campgroundRoutes = require("./routes/campgrounds");
+var indexRoutes = require("./routes/index");
 
-const app               = express();
-const port              = 3000;
+const app = express();
+const port = 3000;
 
-
-mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true, useFindAndModify: false});
+mongoose.connect(
+  "mongodb+srv://YelpCamp:ThisIsThePassword@cluster0-xehsk.mongodb.net/yelp_camp?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useFindAndModify: false }
+);
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
 //seedDB();
 
-app.locals.moment = require('moment');
+app.locals.moment = require("moment");
 
 // PASSPORT CONFIGURATION
-app.use(require("express-session")({
+app.use(
+  require("express-session")({
     secret: "This is the super secret secret",
     resave: false,
     saveUninitialized: false
-}));
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -46,11 +50,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 //settings Local variables
-app.use(function(req,res,next){
-    res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 //Setting up basic paths
@@ -59,4 +63,4 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 //Setting up port
-app.listen(port, () => console.log(`Yelp Camp Page Started on port ${port}!`) );
+app.listen(port, () => console.log(`Yelp Camp Page Started on port ${port}!`));
